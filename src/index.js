dc7c4ce845f257f62f89b73c6855936dcadfe807
Bosterman12 <bandi_alejandro@yahoo.com.ar>
2023-04-22 17:38:51 -0300

    implementando multer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 
 import express from 'express'
 import productRouter from './routes/products.routes.js'
+import { __dirname, __filename } from './path.js'
+import multer from 'multer'
 
 const app = express()
 const PORT = 4000
-
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/public/img')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.originalname}`)
+    }
+}) 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+const upload = (multer({storage:storage}))
+
+
+
+console.log(__dirname)
 
 app.use('/product', productRouter)
+app.use( '/static' ,express.static(__dirname + '/public'))
+app.post('/upload', upload.single('product'), (req, res) => {
+    console.log(req.body)
+    console.log(req.file)
+    res.send("imagen subida")
+})
 
 
 
