566db4498cbe403ff0507ec708ecbe82f3045a06
Bosterman12 <bandi_alejandro@yahoo.com.ar>
2023-04-22 13:16:25 -0300

    product routes implementado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import express from 'express'
-import { ProductManager } from './productmanager.js'
+import productRouter from './routes/products.routes.js'
 
 const app = express()
 const PORT = 4000
@@ -9,41 +9,14 @@ const PORT = 4000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-const productManager = new ProductManager ('./productos.txt')
+app.use('/product', productRouter)
 
 
 
 
-app.get("/product", async (req,res) => {
-    const products = await productManager.getProducts()
-    res.send(products)
-})
 
 
-app.get('/product/:id', async (req, res) => {
-    const product = await productManager.getProductById(req.params.id)
-    res.send(product)
-    
-})
 
-app.post('/product', async (req,res) => {
-    const {title, description, price, thumbnail, code, stock} = req.body
-     await productManager.addProduct({title, description, price, thumbnail, code, stock})
-    res.send("Producto creado")
-})
-
-app.put('/product/:id', async (req,res) => {
-    const id = req.params.id
-    const {title, description, price, thumbnail, code, stock} = req.body
-    const mensaje = await productManager.updateProduct(id,{title, description, price, thumbnail, code, stock})
-    res.send(mensaje)
-})
-
-app.delete('/product/:id', async (req,res) => {
-    const id = req.params.id
-    const mensaje = await productManager.deleteProduct(id)
-    res.send(mensaje)
-})
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
